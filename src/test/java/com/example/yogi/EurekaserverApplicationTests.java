33959eba0d5fb3e342933e2ef3581cfe863c24a7
unknown <owkyoge@gmail.com>
2022-05-21 20:38:30 +0530

    eurekaserver

---
+++ src/test/java/com/example/yogi/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
